5c5ce4844c2b2a1090bf9fe7d85c0765e6b43e85
dg <danny2468@gmail.com>
2023-05-20 20:22:36 -0700

    Parts of segmentation methods

--- VideoProcessTools.py
+++ VideoProcessTools.py
@@ -42,7 +42,8 @@ class SpatialFilter():
     analysis).
     """
     def __init__(self,frame=None,get_bg=True,get_filter=True,
-                 pars={'n':11,'r':5,'ftype':'disk','offset':0,'fillvalue':0}):
+                 pars={'n':11,'r':5,'ftype':'disk','offset':0,
+                       'fillvalue':0,'plot_matrix':False,'verbose':False}):
         self.pars = pars
         if frame is not None:
             self.frame = frame
@@ -70,20 +71,30 @@ class SpatialFilter():
         if pars is not None:
             self.pars.update(pars)
         if self.pars['ftype']=='disk':
-            self.getDiskFilter()
+            self.getDiskFilter(verbose=self.pars['verbose'])
+        if self.pars['plot_matrix']:
+            plt.figure()
+            plt.imshow(self.conv_matrix)
+            plt.pause(1e-1)
+            self.pars.update({'plot_matrix':False})
 
-    def getDiskFilter(self):
+    def getDiskFilter(self,verbose=False):
         self.offset = self.pars['offset']
         self.conv_matrix = np.zeros([self.pars['n'],self.pars['n']])
         self.n_mid = floor(self.pars['n']/2)
-        print(f'Disk filter: using n_mid = {self.n_mid}')
+        if self.pars['r'] is not None:
+            r = self.pars['r']
+        else:
+            r = (self.pars['n']+1)/2
         for irow in range(self.pars['n']):
             for icol in range(self.pars['n']):
                 dist = sqrt( (irow-self.n_mid)**2 + (icol-self.n_mid)**2 );
-                if dist < self.pars['r']-1:
+                if dist < r:
                     self.conv_matrix[irow,icol] = 1.
         self.conv_matrix /= np.sum(self.conv_matrix)
-        print(self.conv_matrix)
+        if verbose:
+            print(f'Disk filter: using n_mid = {self.n_mid}')
+            print(self.conv_matrix)
 
     def apply(self,frame,offset=None,update=True,mode='subtract'):
         self.ret = False
@@ -175,8 +186,7 @@ class FrameSequence():
             self.frame_pointer += self.interval
             if self.fs_type == 'imseq':
                 self.frame = self.image_sequence[self.frame_pointer]
-            elif self.fs_type == 'imfil':
-                self.frame = cv2.imread(os.path.join(self.source_dir, self.image_file_list[self.frame_pointer]))
+            elif self.fs_type == 'imfil':                self.frame = cv2.imread(os.path.join(self.source_dir, self.image_file_list[self.frame_pointer]))
             elif self.fs_type in ['vidseq','vidfil']:
                 for i in range(self.interval):
                     self.ret,self.frame = self.video_sequence.read()
@@ -187,7 +197,100 @@ class FrameSequence():
         self.frame = np.array(self.frame)
         return self.ret,self.frame
 
+
+class Segmenter():
+    def __init__(self,frame=None,pars=None, min_val=minThreshold, max_val=maxThreshold,min_area=minArea,
+                      display_ROIs=False,display_blobs=False,display_blobsCV=False,use_binary=True,
+                             fig_numROI=102,fig_numBLOB=103,fig_numCTR=104):
+        self.frame = frame
+        self.pars = pars
+        self.blobs_fig_num = fig_numBLOB
+        self.ROI_fig_num = fig_numROI
+
+    def getFrame(frame):
+        self.frame = frame
+
+    def binary_frame(self, min_val=minThreshold, max_val=maxThreshold,display=False,fill_holes=True,fig_num=101):
+        self.binary_frame=cv2.threshold(self.frame,self.pars['min_val'],self.pars['max_val'], cv2.THRESH_BINARY)[1]
+        if fill_holes: # Fill holes within thresholded blobs
+            # after example at https://www.programcreek.com/python/example/89425/cv2.floodFill
+            frame_floodfill=self.binary_image.copy()
+            # Mask used to flood filling.
+            # Notice the size needs to be 2 pixels than the image.
+            h, w = self.binary_image.shape[:2]
+            mask = np.zeros((h+2, w+2), np.uint8)
+            # Floodfill from point (0, 0)
+            cv2.floodFill(frame_floodfill, mask, (0,0), 255);
+            # Invert floodfilled image
+            frame_floodfill_inv = cv2.bitwise_not(frame_floodfill)
+            # Combine the two images to get the foreground.
+            self.binary_image = self.binary_image.astype(np.uint8) | frame_floodfill_inv.astype(np.uint8)
+            #print('after:',self.binary_image)
+            self.binary_fig_num=fig_num
+        if display:
+            self.show_binary_frame()
             
+    def show_binary_frame(self):
+        plt.figure(self.binary_fig_num,facecolor=tuple([i/255 for i in bg_color]))
+        plt.imshow(self.binary_image, cmap='gray')
+        plt.tight_layout(pad=plt_pad)
+        #title_str='Figure '+str(self.binary_fig_num)+ \
+        #    ',   Filename: '+self.frame_file
+        #plt.gcf().canvas.manager.set_window_title(title_str)
+    
+                                    
+    def segment(self,bin_frame=None,pars=None):
+        if bin_frame is not None:
+            self.binary_frame = bin_frame
+        if pars is not None:
+            self.pars.update(pars)
+        # 
+        self.ROIlist=[]
+        self.blob_keypoints = []
+
+
+        self.contours, hierarchy = cv2.findContours(self.binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        #self.contours, hierarchy = cv2.findContours(self.binary_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+        if display_blobs:
+            self.show_blobs_frame()
+            
+        # dimensions of image in pixels
+        nx=len(self.frame_image)
+        ny=len(self.frame_image[0])
+
+        # Parse ROIs from contours
+        for ctr in self.contours:
+            #print(ctr[:,0,0])
+            area=cv2.contourArea(ctr)
+            bbox=cv2.boundingRect(ctr)
+            try:
+                ellbox = cv2.fitEllipse(ctr)
+                ell = Ellipse((ellbox[0][0],ellbox[0][1]),ellbox[1][0],ellbox[1][1],angle=ellbox[2],
+                              linewidth=1,edgecolor='y',facecolor='none')
+                if display_ROIs:
+                    plt.gca().add_patch(ell)
+            except:
+                ellbox=None
+            i_beg=np.max([np.min(ctr[:,0,1])-ROIpad,0])
+            i_end=np.min([np.max(ctr[:,0,1])+ROIpad,nx-1])
+            j_beg=np.max([np.min(ctr[:,0,0])-ROIpad,0])
+            j_end=np.min([np.max(ctr[:,0,0])+ROIpad,ny-1])
+            
+            # get blob subimage
+            blob_img = Image.fromarray( self.color_frame_image[i_beg:i_end, j_beg:j_end])
+            #blob_img = Image.fromarray( cv2.cvtColor(self.color_frame_image[i_beg:i_end, j_beg:j_end], cv2.COLOR_BGR2RGB))
+            #blob_img = Image.fromarray( cv2.cvtColor(self.frame_image[i_beg:i_end, j_beg:j_end], cv2.COLOR_BGR2RGB))
+            #blob_img = Image.fromarray( cv2.cvtColor(self.frame_image[i_beg:i_end, j_beg:j_end], cv2.COLOR_BGR2GRAY))
+            
+            self.ROIlist.append(ROI(ROIimage=blob_img,edge=np.squeeze(ctr,axis=1),
+                                    area=area,bbox=bbox,ellbox=ellbox,
+                                    i_beg=i_beg,i_end=i_end,j_beg=j_beg,j_end=j_end,
+                                    category=category))    
+        if display_ROIs:
+            self.show_ROIs_frame()
+
+    
 class VideoProcessor():
     """
     A class implementing background subtraction and other processing of
